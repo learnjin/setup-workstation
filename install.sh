a9f10d352a2f11dcffeb7982871c3e5cf7142863
Kai Rubarth <krubar@gmail.com>
2013-10-02 12:50:34 +0200

    install update

--- install.sh
+++ install.sh
@@ -3,17 +3,17 @@ set -e
 set -u
 
 # display everything on stderr.
-exec 1>&2
+#exec 1>&2
 
 TARBALL_URL="https://github.com/learnjin/setup-workstation/archive/master.tar.gz"
 
-INSTALL_TMPDIR="$HOME/.setup-workstation-tmp"
+INSTALL_TMPDIR="$HOME/.workstation-tmp"
 rm -rf "$INSTALL_TMPDIR"
 mkdir "$INSTALL_TMPDIR"
 echo "Downloading workstation setup"
-curl --progress-bar --fail "$TARBALL_URL" | tar -xzf - -C "$INSTALL_TMPDIR"
+curl --progress-bar --fail -L "$TARBALL_URL" | tar --strip 1 -xzf - -C "$INSTALL_TMPDIR"
 # bomb out if it didn't work, eg no net
-test -x "${INSTALL_TMPDIR}/workstation/after-login.sh"
+test -x "${INSTALL_TMPDIR}/.workstation/after-login.sh"
 mv "${INSTALL_TMPDIR}/.workstation" "$HOME"
 rmdir "${INSTALL_TMPDIR}"
 # just double-checking :)
